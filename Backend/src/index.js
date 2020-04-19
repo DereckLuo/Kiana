e47685931f87d0da76bd1e51969bc139a5ae25c3
Chongxin Luo <Chongxin.Luo@ibm.com>
2020-04-19 16:07:39 -0400

    Setup backend express
    * setup the backend express server. Able to receive curl command and
      send back correct status code and message.
    
    Signed-off-by: Chongxin Luo <Chongxin.Luo@ibm.com>

---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+    return res.status(200).send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.status(200).send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.status(200).send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+    console.log(`Kiana listening on port ${process.env.PORT}!`),
+);
